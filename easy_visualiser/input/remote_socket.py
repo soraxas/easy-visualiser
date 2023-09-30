d4527c098b11fd1ee9ee1f3655e8ccadd7b7cd00
Tin Lai <oscar@tinyiu.com>
2023-10-01 01:38:24 +1000

    implements property and attribute access
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- easy_visualiser/input/remote_socket.py
+++ easy_visualiser/input/remote_socket.py
@@ -1,4 +1,5 @@
 import asyncio
+import enum
 import inspect
 import os
 import queue
@@ -19,6 +20,11 @@ from . import DataSourceSingleton
 class PyroDaemonIO:
     input_queue = queue.Queue()
     output_queue = queue.Queue()
+
+
+class PyroRemoteCallType(enum.Enum):
+    method_call = enum.auto()
+    attribute_access = enum.auto()
 
 
 class MyPyroDaemon(threading.Thread):
@@ -53,19 +59,29 @@ class MyPyroDaemon(threading.Thread):
 
         @Pyro5.api.expose
         class PyroAdapter:
-            def message(__self, messagetext, args=tuple(), kwargs={}):
-                logger.trace("requesting {}", messagetext)
-                self.queue_io.input_queue.put((messagetext, args, kwargs))
+            def method_call(__self, method_name, args=tuple(), kwargs={}):
+                __self.method_call_nowait(method_name, args, kwargs)
                 ok = self.queue_io.output_queue.get()
                 logger.trace("got {}", ok)
                 return ok
 
-            def message_nowait(__self, messagetext, args=tuple(), kwargs={}):
-                logger.trace("requesting {}", messagetext)
-                self.queue_io.input_queue.put((messagetext, args, kwargs))
+            def method_call_nowait(__self, method_name, args=tuple(), kwargs={}):
+                logger.trace("requesting {}", method_name)
+                self.queue_io.input_queue.put(
+                    (PyroRemoteCallType.method_call, (method_name, args, kwargs))
+                )
+
+            def attribute_access(__self, attribute_name):
+                logger.trace("requesting attr {}", attribute_name)
+                self.queue_io.input_queue.put(
+                    (PyroRemoteCallType.attribute_access, attribute_name)
+                )
+                ok = self.queue_io.output_queue.get()
+                logger.trace("got {}", ok)
+                return ok
 
             def __bool__(__self):
-                return __self.message("__bool__")
+                return __self.method_call("__bool__")
 
         self.uri = daemon.register(PyroAdapter, "easy_visualiser.Visualiser")
         print(self.uri)
@@ -101,12 +117,15 @@ class RemoteControlProxyDatasource(DataSourceSingleton):
 
         while self.visualiser:
             logger.trace("retrieving request")
-            msg = self.queue_io.input_queue.get()
-            logger.trace("got request: {}", msg)
+            call_type, msg = self.queue_io.input_queue.get()
+            logger.trace("got request: {} ({})", msg, call_type)
 
-            method_name, args, kwargs = msg
+            if call_type is PyroRemoteCallType.method_call:
+                method_name, args, kwargs = msg
 
-            out = getattr(self.visualiser, method_name)(*args, **kwargs)
+                out = getattr(self.visualiser, method_name)(*args, **kwargs)
+            elif call_type is PyroRemoteCallType.attribute_access:
+                out = getattr(self.visualiser, msg)
 
             self.queue_io.output_queue.put(out)
 
@@ -125,13 +144,13 @@ class EasyVisualiserClientProxy:
         self.visualiser_server = Pyro5.api.Proxy(self.uri)
 
     def __getattr__(self, name):
-        datapack = dict()
         try:
             _attribute = getattr(Visualiser, name)
         except AttributeError:
             # maybe it's an object variable that only exists after initialisation?
-            raise NotImplementedError(f"{name}")
-            return None
+            return self.visualiser_server.attribute_access(name)
+            # raise NotImplementedError(f"{name}")
+            # return None
         else:
             logger.debug("Got attr: {}", _attribute)
             logger.opt(lazy=True).debug(
@@ -142,13 +161,14 @@ class EasyVisualiserClientProxy:
             )
 
             if inspect.isfunction(_attribute):
-                datapack["callable"] = _attribute
+                return lambda *args, **kwargs: self.visualiser_server.method_call(
+                    name, args, kwargs
+                )
+            elif inspect.isdatadescriptor(_attribute):
+                return self.visualiser_server.attribute_access(name)
             else:
-                print(type(_attribute))
-                print(_attribute)
-
-            return lambda *args, **kwargs: self.visualiser_server.message(
-                name, args, kwargs
-            )
+                # print(type(_attribute))
+                # print(_attribute)
+                raise NotImplementedError(f"{_attribute}")
 
         return super().__getattr__(name)
