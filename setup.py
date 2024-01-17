e8ad228b0f386177f296cba3524961515f85758a
Tin Lai <oscar@tinyiu.com>
2024-01-17 13:59:49 +1100

    bump version
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="easy-visualiser",
-    version="0.1.1",
+    version="0.1.2",
     author="Tin Lai",
     author_email="oscar@tinyiu.com",
     description="Pluggable visualiser",
