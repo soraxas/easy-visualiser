e866751859ec2b4c2b1a07b591901d1a84a7d7bf
Tin Lai <oscar@tinyiu.com>
2024-01-17 13:43:36 +1100

    fix import issue
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- easy_visualiser/plugins/__init__.py
+++ easy_visualiser/plugins/__init__.py
@@ -4,10 +4,13 @@ from .abstract_visualisable_plugin import (
 )
 from .functional_zscaler import AxisScalerPlugin
 from .visualisable_2D_lineplot import Visualisable2DLinePlot
-from .visualisable_auto_status_bar import VisualisableAutoStatusBar
 from .visualisable_axis import VisualisablePrincipleAxis
 from .visualisable_gridlines import VisualisableGridLines
 from .visualisable_image import VisualisableImage
-from .visualisable_message_board import VisualisableMessageBoard, VisualisableStatusBar
+from .visualisable_message_board import (
+    VisualisableAutoStatusBar,
+    VisualisableMessageBoard,
+    VisualisableStatusBar,
+)
 from .visualisable_points import VisualisablePoints
 from .visualisable_volumeplot import VisualisableVolumePlot
--- easy_visualiser/plugins/visualisable_message_board.py
+++ easy_visualiser/plugins/visualisable_message_board.py
@@ -58,3 +58,13 @@ class VisualisableStatusBar(VisualisableMessageBoard):
     def clear_lines(self):
         self.lines.clear()
         self.update_status()
+
+
+class VisualisableAutoStatusBar(VisualisableMessageBoard):
+    def on_initialisation(self, *args, **kwargs):
+        super().on_initialisation(*args, **kwargs)
+        self.visualiser.hooks.on_initialisation_finish.add_hook(self.update_status)
+        self.visualiser.hooks.on_keypress_finish.add_hook(self.update_status)
+
+    def update_status(self):
+        self.set_message(self.visualiser.current_modal.state.to_help_msg())
